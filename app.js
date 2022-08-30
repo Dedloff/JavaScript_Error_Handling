a1c8abf14b861db8eaf7bd327ebc3703c86a452f
Dedloff <dedloffn@gmail.com>
2022-08-29 21:39:28 -0600

    Added answer

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+"use strict";
+
 // TODO: Enable strict mode
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+function parseToJSON(data) {
+  try {
+    // TODO: Add a try/catch block to
+    // attempt to convert 'data' to JSON
+    return JSON.parse(data);
+  } catch (error) {
+    // TODO: if an exception is raised
+    // print the error to the console
+    // and return null
+    console.error(error);
+    return null;
+  }
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
